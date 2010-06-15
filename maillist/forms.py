f082e050012ee5fed5fdc6599785949ea6553bb8
Sidorenko Vladimir <yoyavova@gmail.com>
2010-06-15 12:09:30 +0300

    translations

--- maillist/forms.py
+++ maillist/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.utils.translation import ugettext as _
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(_("email"))
+    email = forms.EmailField(label=_("email"))
 
 class ImageForm(forms.Form):
     file = forms.ImageField()
