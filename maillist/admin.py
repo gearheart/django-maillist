73021c11114d7a5c6533c8f2161b23424b8d8f6d
Sidorenko Vladimir <yoyavova@gmail.com>
2010-06-15 08:26:43 +0300

    models and admin

---
+++ maillist/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.db.models import TextField
+from django.utils.translation import ugettext as _
+
+from maillist import models
+from maillist.widgets import JQueryEditor
+
+class MaillistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subscrubers_count', 'last_email')
+
+    def last_email(self, obj):
+        last_email = obj.mails.all()[:1]
+        if len(last_email):
+            return '%s (%s)' % (last_email[0].publish_date, last_email[0].name)
+
+        return _('Mail list is empty')
+
+    def subscrubers_count(self, obj):
+        return obj.subscribers.count()
+    subscrubers_count.short_description = _('subscrubers count')
+
+
+class MailAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'create_date', 'publish_date', 'maillist')
+    list_filter = ('maillist', )
+
+admin.site.register(models.Maillist, MaillistAdmin)
+admin.site.register(models.Mail, MailAdmin)
+
---
+++ maillist/models.py
@@ -0,0 +1,70 @@
+import datetime
+
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class Maillist(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    description = models.TextField(_('description'))
+
+    class Meta:
+        verbose_name = _('Mail list')
+        verbose_name_plural = _('Mail lists')
+
+    def __unicode__(self):
+        return self.name
+
+class Mail(models.Model):
+    maillist = models.ForeignKey(Maillist, verbose_name=_('maillist'),
+                                                        related_name='mails')
+    subject = models.CharField(_('subject'), max_length=100)
+    content = models.TextField(_('content'))
+
+    create_date = models.DateTimeField(editable=False)
+    publish_date = models.DateTimeField(blank=True, default=datetime.datetime.now)
+
+    class Meta:
+        verbose_name = _('Mail')
+        verbose_name_plural = _('Mails')
+        ordering = ('-publish_date', )
+
+
+    def __unicode__(self):
+        return self.subject
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.create_date = datetime.datetime.now()
+        if not self.publish_date:
+            self.publish_date = self.create_date
+        super(Mail, self).save(*args, **kwargs)
+
+
+class Subscriber(models.Model):
+    maillist = models.ForeignKey(Maillist, verbose_name=_('maillist'),
+                                                    related_name='subscribers')
+    email = models.EmailField()
+    subscription_date = models.DateTimeField(editable=False)
+
+    class Meta:
+        verbose_name = _('Subscriber')
+        verbose_name_plural = ('Subscribers')
+        unique_together = (
+            ('email', 'maillist'),
+        )
+
+    def __unicode__(self):
+        return self.email
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.subscription_date = datetime.datetime.now()
+        super(Subscriber, self).save(*args, **kwargs)
+
+
+class EmailLog(models.Model):
+    mail = models.ForeignKey(Mail, related_name='logs')
+    subscriber = models.ForeignKey(Subscriber, related_name='logs')
+
+    success = models.BooleanField()
